b5426fb9d15d618ecebe40328ff5fa1631249aef
Tewabe Tadege <ethiohabesha412@gmail.com>
2022-05-16 20:18:31 -0700

    Redux added to Components

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,44 +1,48 @@
 import React, { Component } from 'react';
-import {Text, ScrollView, FlatList } from 'react-native';
-import { Card, ListItem  } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
+import { ScrollView, Text, FlatList } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
+function Mission(){
+    return (
+        <Card title="Mission" wrapperStyle={{margin: 20}}
+        > 
+            <Text style={{margin: 10}}> 
+            We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+            </Text>
+        </Card>
+    )
+}
 class About extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        };
-    }
 
     static navigationOptions = {
         title: 'About Us'
     }
     render() {
 
-        const renderPartnerItem= ({item}) => {
+        const renderPartner= ({item}) => {
             return (
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
         };
 
         return (
             <ScrollView>
-                <Card title="Mission" wrapperStyle={{margin: 20}}
-                > 
-                    <Text style={{margin: 10}}> 
-                    We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
-                    </Text>
-                </Card>
-                <Card title="Community Partners" wrapperStyle={{margin: 20}}>
-                    <FlatList
-                        data={this.state.partners}
-                        renderItem={renderPartnerItem}
+            <Mission />
+                <Card title="Community Partners">
+                    <FlatList 
+                        data={this.props.partners.partners}
+                        renderItem={renderPartner}
                         keyExtractor={item => item.id.toString()}
                     />
                 </Card>
@@ -46,4 +50,4 @@ class About extends Component {
         );
     }
 }
-export default About;
+export default connect(mapStateToProps)(About);
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://192.168.86.23.:3001/';
+export const baseUrl = 'http://192.168.86.23:3001/';
