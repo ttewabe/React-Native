0c1a0981ced9036ad4139ae9b6556922428cbc3f
Tewabe Tadege <ethiohabesha412@gmail.com>
2022-05-23 23:21:43 -0700

    Animation & Animatable

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,18 +1,13 @@
 import React, { Component} from 'react';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
-import {
-    Modal,
-    Button,
-    StyleSheet,
-    Text,
-    View,
-    ScrollView,
-    FlatList,
-} from 'react-native';
+import {Modal, Button, StyleSheet, Text, View, ScrollView, FlatList} from 'react-native';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postComment, postFavorite } from '../redux/ActionCreators';
 import { comments } from '../redux/comments';
+import * as Animatable from 'react-native-animatable';
+
+
 const mapStateToProps = (state) => {
     return {
         campsites: state.campsites,
@@ -29,34 +24,36 @@ function RenderCampsite(props) {
     const { campsite } = props;
     if (campsite) {
         return (
-            <Card
-                featuredTitle={campsite.name}
-                image={{ uri: baseUrl + campsite.image }}
-            >
-                <Text style={{ margin: 10 }}>{campsite.description}</Text>
-                <View style={styles.cardRow}>
-                    <Icon
-                        name={props.favorite ? 'heart' : 'heart-o'}
-                        type="font-awesome"
-                        color="#f50"
-                        raised
-                        reverse
-                        onPress={() =>
-                            props.favorite
-                                ? console.log('Already set as a favorite')
-                                : props.markFavorite()
-                        }
-                    />
-                    <Icon
-                        name="pencil"
-                        type="font-awesome"
-                        color="#5637DD"
-                        raised
-                        reverse
-                        onPress={() => props.onShowModal()}
-                    />
-                </View>
-            </Card>
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Card
+                    featuredTitle={campsite.name}
+                    image={{ uri: baseUrl + campsite.image }}
+                >
+                    <Text style={{ margin: 10 }}>{campsite.description}</Text>
+                    <View style={styles.cardRow}>
+                        <Icon
+                            name={props.favorite ? 'heart' : 'heart-o'}
+                            type="font-awesome"
+                            color="#f50"
+                            raised
+                            reverse
+                            onPress={() =>
+                                props.favorite
+                                    ? console.log('Already set as a favorite')
+                                    : props.markFavorite()
+                            }
+                        />
+                        <Icon
+                            name="pencil"
+                            type="font-awesome"
+                            color="#5637DD"
+                            raised
+                            reverse
+                            onPress={() => props.onShowModal()}
+                        />
+                    </View>
+                </Card>
+            </Animatable.View>
         );
     }
     return <View />;
@@ -84,13 +81,15 @@ function RenderComments({ comments }) {
         );
     };
     return (
-        <Card title="Comments">
-            <FlatList
-                data={comments}
-                renderItem={renderCommentItem}
-                keyExtractor={(item) => item.id.toString()}
-            />
-        </Card>
+        <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
+            <Card title="Comments">
+                <FlatList
+                    data={comments}
+                    renderItem={renderCommentItem}
+                    keyExtractor={(item) => item.id.toString()}
+                />
+            </Card>
+        </Animatable.View>
     );
 }
 class CampsiteInfo extends Component {
--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text, StyleSheet} from 'react-native';
+import { FlatList, View, Text, StyleSheet, Alert} from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
@@ -7,6 +7,7 @@ import { baseUrl } from '../shared/baseUrl';
 import { SwipeRow } from 'react-native-swipe-list-view';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { deleteFavorite } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
 
 
 const mapStateToProps = state => {
@@ -32,10 +33,29 @@ class Favorites extends Component {
             return (
                 <SwipeRow rightOpenValue={-100} style={styles.swipeRow}>
                     <View style={styles.deleteView}>
-                        <TouchableOpacity
-                        style={styles.deleteTouchable}
-                        onPress={() => this.props.deleteFavorite(item.id)}
-                        >
+                    <TouchableOpacity
+                    style={styles.deleteTouchable}
+                    onPress={() =>
+                        Alert.alert(
+                            'Delete Favorite?',
+                            'Are you sure you wish to delete the favorite campsite ' +
+                                item.name +
+                                '?',
+                            [
+                                {
+                                    text: 'Cancel',
+                                    onPress: () => console.log(item.name + 'Not Deleted'),
+                                    style: 'cancel'
+                                },
+                                {
+                                    text: 'OK',
+                                    onPress: () => this.props.deleteFavorite(item.id)
+                                },
+                            ],
+                            { cancelable: false }
+                        )
+                    }
+                >
                         <Text style={styles.deleteText}>Delete</Text>
                         </TouchableOpacity>
                     </View>
@@ -63,13 +83,15 @@ class Favorites extends Component {
             );
         }
         return (
-            <FlatList
-                data={this.props.campsites.campsites.filter(
-                    campsite => this.props.favorites.includes(campsite.id)
-                )}
-                renderItem={renderFavoriteItem}
-                keyExtractor={item => item.id.toString()}
-            />
+            <Animatable.View animation="fadeInRightBig" duration={2000}>
+                <FlatList
+                    data={this.props.campsites.campsites.filter(
+                        campsite => this.props.favorites.includes(campsite.id)
+                    )}
+                    renderItem={renderFavoriteItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+            </Animatable.View>
         );
     }
 }
